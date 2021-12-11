29c4242e0ce04bfdd391754f59739703deca4ba4
keita_kawaguchi <keikawagu@gmail.com>
2021-12-11 11:19:31 +0900

    メモページコンポーネントを作成

--- src/App.js
+++ src/App.js
@@ -1,35 +1,9 @@
-import React, { useState } from 'react';
+import MemoPage from "./pages/TheMemo";
 
-function App() {
-  const [memos, setState] = useState(["memo1", "memo2", "memo3"]);
-  const [text, setText] = useState("");
-
-  const lists = memos.map(memo => {
-    return(
-      <li>{ memo }</li>
-    )
-  });
-  
-  const handleChange = (e) => {
-    setText(() => e.target.value);
-  }
-
-const saveMemo = () => {
-  const addedMemos = memos.slice();
-  addedMemos.push(text);
-  setState(() => addedMemos);
-}
-
+const App = () => {
   return (
     <>
-      <h1>Memo App for React</h1>
-      <input type="text" value={text} onChange={handleChange} />
-      <button onClick={saveMemo}>保存</button>
-      <div>
-        <ul>
-          { lists }
-        </ul>
-      </div>
+      <MemoPage />
     </>
   );
 }
---
+++ src/pages/TheMemo.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+
+const Memo = () => {
+  const [memos, setState] = useState(["memo1", "memo2", "memo3"]);
+  const [text, setText] = useState("");
+
+  const lists = memos.map(memo => {
+    return(
+      <li>{ memo }</li>
+    )
+  });
+  
+  const handleChange = (e) => {
+    setText(() => e.target.value);
+  }
+
+const saveMemo = () => {
+  const addedMemos = memos.slice();
+  addedMemos.push(text);
+  setState(() => addedMemos);
+}
+
+  return (
+    <>
+      <h1>Memo App for React</h1>
+      <input type="text" value={text} onChange={handleChange} />
+      <button onClick={saveMemo}>保存</button>
+      <div>
+        <ul>
+          { lists }
+        </ul>
+      </div>
+    </>
+  );
+}
+
+export default Memo;
