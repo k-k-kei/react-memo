9b3af138386318c733f97a5164c9717e081be5cc
keita_kawaguchi <keikawagu@gmail.com>
2021-12-11 12:15:35 +0900

    Material-UIのセッティング完了

--- src/index.js
+++ src/index.js
@@ -6,6 +6,10 @@ import App from './App';
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    
+    {/* Material-UIのGoogle FontとFont Iconsをダウンロード */}
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
   </React.StrictMode>,
   document.getElementById('root')
 );
